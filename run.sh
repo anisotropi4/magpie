b268eb79443e27cc5dc1b1fc6b6861f753d842a8
Will Deakin <will.deakin@crinstitute.org.uk>
2020-05-25 21:39:32 +0100

    create all tags, rail and electrification features

--- run.sh
+++ run.sh
@@ -13,27 +13,50 @@ if [ ! -s  output/${FILENAME} ]; then
     curl -L -o output/${FILENAME} ${URL}/${FILENAME}
 fi
 
-
-if [ ! -f ${REGION}.poly ]; then
-    echo missing ${REGION}.poly polygon file
+if [ ! -f data/${REGION}.poly ]; then
+    echo missing data/${REGION}.poly polygon file
     exit 1
 fi
 
 if [ x"$(find output/${REGION}-latest.osm.pbf -mmin -15)" = x ]; then
-    osmupdate output/${FILENAME} ${REGION}-update.osm.pbf -B=${REGION}.poly --verbose --keep-tempfiles
+    osmupdate output/${FILENAME} ${REGION}-update.osm.pbf -B=data/${REGION}.poly --verbose --keep-tempfiles
     mv output/${FILENAME} output/archive
     mv ${REGION}-update.osm.pbf output/${FILENAME}
 else
     echo "${FILENAME} is less than 15 minutes old"
 fi
 
-KEYWORD=rail
 for element in points lines multilinestrings multipolygons other_relations
 do
+    KEYWORD=all
     if [ ! -s ${REGION}-${KEYWORD}-${element}.json ]; then
-        ogr2ogr --config OGR_INTERLEAVED_READING YES --config OSM_CONFIG_FILE osmconf-all.ini \
+        ogr2ogr --config OGR_INTERLEAVED_READING YES --config OSM_CONFIG_FILE osmconf-${KEYWORD}.ini \
                 -where "(railway IS NOT NULL) OR (train IS NOT NULL) or (rail IS NOT NULL)" \
 		            -f GeoJSON ${REGION}-${KEYWORD}-${element}.json \
 		            output/${REGION}-latest.osm.pbf ${element}
     fi
 done
+
+if [ ! -s extract-keys.txt ]; then
+    jq '.features[].properties.all_tags' great-britain-all-*.json | sed 's/=>/:/g; s/^"//; s/"$//; s/\\//g; s/","/"\n"/g' | sed 's/":.*$/"/' | sort -u > extract-keys.txt
+fi
+
+
+for element in points lines multilinestrings multipolygons other_relations
+do
+    KEYWORD=rail
+    if [ ! -s ${REGION}-${KEYWORD}-${element}.json ]; then
+            ogr2ogr --config OGR_INTERLEAVED_READING YES --config OSM_CONFIG_FILE osmconf-${KEYWORD}.ini \
+                -where "(railway IS NOT NULL) OR (train IS NOT NULL) or (rail IS NOT NULL)" \
+		            -f GeoJSON ${REGION}-${KEYWORD}-${element}.json \
+		            output/${REGION}-latest.osm.pbf ${element}
+    fi
+    KEYWORD=voltage
+    if [ ! -s ${REGION}-${KEYWORD}-${element}.json ]; then
+            ogr2ogr --config OGR_INTERLEAVED_READING YES --config OSM_CONFIG_FILE osmconf-${KEYWORD}.ini \
+                -where "((railway IS NOT NULL) OR (train IS NOT NULL) or (rail IS NOT NULL)) AND (voltage IS NOT NULL)" \
+		            -f GeoJSON ${REGION}-${KEYWORD}-${element}.json \
+		            output/${REGION}-latest.osm.pbf ${element}
+    fi
+done
+
