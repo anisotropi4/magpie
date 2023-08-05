bb330419c28756af463d9e7aad733e49fbe10e58
Will Deakin <will.deakin@crinstitute.org.uk>
2023-08-05 14:05:07 +0100

    refactor with get and simplify functions

--- osmnxget.py
+++ osmnxget.py
@@ -5,6 +5,8 @@ from functools import partial
 
 import osmnx as ox
 import pandas as pd
+import geopandas as gp
+import numpy as np
 from osmnx.utils import log
 from pyogrio import read_dataframe, write_dataframe
 from shapely import set_precision
@@ -24,13 +26,24 @@ pd.set_option("display.max_columns", None)
 
 set_precision_one = partial(set_precision, grid_size=1.0)
 
+def get_node_group(this_graph, key):
+    _, edge = ox.graph_to_gdfs(this_graph)
+    r = edge[["railway"]].groupby(key)
+    group = dict(list(r))
+    r = {}
+    for k, v in group.items():
+        node_id = v.reset_index().values[:, 0:2].reshape(-1)
+        r[k] = np.unique(node_id)
+    return r
 
-def main():
-    log("start")
-    polygon = read_dataframe("data/britain-simple.geojson")
-    polygon = polygon.explode(index_parts=False).reset_index(drop=True)
-    polygon = polygon.loc[polygon.area.sort_values(ascending=False).index]
-    polygon = polygon.to_crs(WGS84).iloc[0, 0]
+def get_simplified_nx(this_graph, node_id, key):
+    r = this_graph.subgraph(node_id)
+    r = ox.simplify_graph(r)
+    node, edge = ox.graph_to_gdfs(r)
+    edge["railway"] = key
+    return node, edge
+
+def get_network(polygon):
     log("download railway")
     ox.settings.useful_tags_node = (
         "ref,changeset,id,version,uid,ref:tiploc,name,electrified,frequency,voltage,railway,layer,"
@@ -40,18 +53,13 @@ def main():
         "bridge,tunnel,width,id,maxspeed,junction,name,version,uid,changeset,landuse,timestamp,"
         "user,ref,oneway,ref:tiploc,name,electrified,frequency,voltage,railway,layer,level"
     ).split(",")
-    # tags = (
-    #    "rail|subway|light_rail|tram|narrow_gauge|disused|abandoned|construction|razed|"
-    #    "dismantled"
-    # ).split("|")
-    # railway = ox.features.features_from_polygon(polygon, {"railway": tags})
+    include = (
+        "rail|proposed|construction|disused|abandoned|razed|narrow_gauge|light_rail|subway|"
+        "tram"
+    )
     exclude = (
         "highway|cycleway|footway|path|pedestrian|steps|corridor|elevator|escalator|bridleway|"
-        "platform|raceway|service|subway_entrance|disused_station"
-    )
-    include = (
-        "rail|subway|light_rail|tram|narrow_gauge|disused|abandoned|construction|razed|"
-        "dismantled"
+        "platform|raceway|service|subway_entrance|disused_station|tram_stop"
     )
     custom_filter = f'["railway"~"{include}"]["railway"!~"{exclude}"]'
     log("Create network")
@@ -59,43 +67,38 @@ def main():
         polygon, simplify=False, retain_all=True, custom_filter=custom_filter
     )
     log("Create GeoPandas dataframe")
-    node, edge = ox.graph_to_gdfs(railway)
+    rail_nodes = get_node_group(railway, "railway")
+    node, edge = gp.GeoDataFrame(), gp.GeoDataFrame()
+    for k, v in rail_nodes.items():
+        log(k)
+        i, j = get_simplified_nx(railway, v, k)
+        i, j = i.reset_index().fillna(""), j.reset_index().fillna("")
+        node = pd.concat([i, node])
+        edge = pd.concat([j, edge])
+    return node, edge
+
+def main():
+    log("start")
+    polygon = read_dataframe("data/britain-simple.geojson")
+    polygon = polygon.explode(index_parts=False).reset_index(drop=True)
+    polygon = polygon.loc[polygon.area.sort_values(ascending=False).index]
+    polygon = polygon.to_crs(WGS84).iloc[0, 0]
+    node, edge = get_network(polygon)
     r = node.reset_index().fillna("").to_crs(CRS)
     r["geometry"] = r["geometry"].map(set_precision_one)
-    #ix = r.within(polygon) | r.intersects(polygon)
-    #r["location"] = "GB"
-    #r.loc[~ix, "location"] = "-"
-    log("Output GeoPKG full nodes")
-    write_dataframe(r.to_crs(CRS), OUTPATH, layer="full_node")
-    r = edge.reset_index().fillna("").to_crs(CRS)
-    r["geometry"] = r["geometry"].map(set_precision_one)
-    #ix = r.within(polygon) | r.intersects(polygon)
-    #r["location"] = "GB"
-    #r.loc[~ix, "location"] = "-"
-    log("Output GeoPKG full lines")
-    write_dataframe(r.to_crs(CRS), OUTPATH, layer="full_line")
-    log("Simplify network")
-    railway = ox.simplify_graph(railway)
-    log("Create GeoPandas dataframe")
-    node, edge = ox.graph_to_gdfs(railway)
     r = node.reset_index().fillna("").to_crs(CRS)
     r["geometry"] = r["geometry"].map(set_precision_one)
-    #ix = r.within(polygon) | r.intersects(polygon)
-    #r["location"] = "GB"
-    #r.loc[~ix, "location"] = "-"
     log("Output GeoPKG nodes")
     node["geometry"] = node["geometry"].map(set_precision_one)
     write_dataframe(r.to_crs(CRS), OUTPATH, layer="node")
     r = edge.reset_index().fillna("").to_crs(CRS)
     r["geometry"] = r["geometry"].map(set_precision_one)
-    #ix = r.within(polygon) | r.intersects(polygon)
-    #r["location"] = "GB"
-    #r.loc[~ix, "location"] = "-"
     log("Output GeoPKG lines")
     write_dataframe(r.to_crs(CRS), OUTPATH, layer="line")
     output = r.to_json(na="drop", drop_id=True)
     with open("great-britain-rail.geojson", "w", encoding="utf8") as fout:
         fout.write(output)
+    log("finish")
 
 
 if __name__ == "__main__":
